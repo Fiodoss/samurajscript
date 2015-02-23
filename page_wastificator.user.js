a08e028db3e3c3d36cd887b4886734731b177e4f
Ilgiz Mustafin <ilgimustafin@gmail.com>
2015-02-23 20:52:52 +0300

    PageWastificator uses styles.user.js file

--- page_wastificator.user.js
+++ page_wastificator.user.js
@@ -1,67 +1,15 @@
 // ==UserScript==
 // @name        PageWastificator
 // @namespace   ru.recursivepie.pagewastificator
+// @require 	https://raw.githubusercontent.com/RecursivePie/Wastificator/master/styles.js
 // @include     *
-// @version     0.1
+// @version     0.1.1
 // @grant       none
 //
 // @author      RecursivePie (recursivepie@gmail.com)
 // ==/UserScript==
 
-var style = {
-	replaces : {
-		'А' : 'A',
-		'Б' : '6',
-		'В' : 'B',
-		'Г' : 'F',
-		'Д' : 'D',
-		'Е' : 'E',
-		'Ё' : 'E',
-		'Ж' : 'X',
-		'З' : '3',
-		'И' : 'N',
-		'Й' : 'N',
-		'К' : 'K',
-		'Л' : 'Л',
-		'М' : 'M',
-		'Н' : 'H',
-		'О' : '0',
-		'П' : 'П',
-		'Р' : 'P',
-		'С' : 'C',
-		'Т' : 'T',
-		'У' : 'Y',
-		'Ф' : 'Ф',
-		'Х' : 'X',
-		'Ц' : 'U',
-		'Ч' : '4',
-		'Ш' : 'W',
-		'Щ' : 'W',
-		'Ь' : 'b',
-		'Ы' : 'bI',
-		'Ъ' : 'b',
-		'Э' : '3',
-		'Ю' : 'I0',
-		'Я' : 'R'
-	},
-	getWastified : function(s) {
-		var ans = "";
-		var rep = this.replaces;
-		var englishRegexp = /[a-z]/;
-		for (var i = 0; i < s.length; i++) {
-			var c = s[i];
-			if (!englishRegexp.test(c)) {
-				c = c.toUpperCase();
-			}
-			if (rep[c] === null || rep[c] === undefined) {
-				ans += c;
-			} else {
-				ans += rep[c];
-			}
-		}
-		return ans;
-	}
-};
+var style = styles["ps1_lite"];
 
 function kek(str) {
 	return style.getWastified(str);
