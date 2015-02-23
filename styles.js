ce61d490406b9d696c0ac9b083e54a8eb9105dd3
Ilgiz Mustafin <ilgimustafin@gmail.com>
2015-02-23 12:25:15 +0300

    Initial commit. Starting v6.3

---
+++ styles.js
@@ -0,0 +1,315 @@
+var styles = {
+	"ps1" : {
+		replaces : {
+			'А' : 'A',
+			'Б' : '6',
+			'В' : 'B',
+			'Г' : '7',
+			'Д' : 'D',
+			'Е' : 'E',
+			'Ё' : 'E',
+			'Ж' : 'X',
+			'З' : '3',
+			'И' : 'N',
+			'Й' : 'N',
+			'К' : 'K',
+			'Л' : 'JI',
+			'М' : 'M',
+			'Н' : 'H',
+			'О' : 'Ф',
+			'П' : 'II',
+			'Р' : 'P',
+			'С' : 'C',
+			'Т' : 'T',
+			'У' : 'Y',
+			'Ф' : 'Ф',
+			'Х' : 'X',
+			'Ц' : 'U',
+			'Ч' : '4',
+			'Ш' : 'W',
+			'Щ' : 'W',
+			'Ь' : 'b',
+			'Ы' : 'bI',
+			'Ъ' : 'b',
+			'Э' : '3',
+			'Ю' : 'I0',
+			'Я' : '9'
+		},
+		getWastified : function(s) {
+			var ans = "";
+			var rep = this.replaces;
+			var englishRegexp = /[a-z]/;
+			for (var i = 0; i < s.length; i++) {
+				var c = s[i];
+				if (!englishRegexp.test(c)) {
+					c = c.toUpperCase();
+				}
+				if (rep[c] === null || rep[c] === undefined) {
+					ans += c;
+				} else {
+					ans += rep[c];
+				}
+			}
+			return ans;
+		}
+	},
+	"ps1_lite" : {
+		replaces : {
+			'А' : 'A',
+			'Б' : '6',
+			'В' : 'B',
+			'Г' : 'F',
+			'Д' : 'D',
+			'Е' : 'E',
+			'Ё' : 'E',
+			'Ж' : 'X',
+			'З' : '3',
+			'И' : 'N',
+			'Й' : 'N',
+			'К' : 'K',
+			'Л' : 'Л',
+			'М' : 'M',
+			'Н' : 'H',
+			'О' : '0',
+			'П' : 'П',
+			'Р' : 'P',
+			'С' : 'C',
+			'Т' : 'T',
+			'У' : 'Y',
+			'Ф' : 'Ф',
+			'Х' : 'X',
+			'Ц' : 'U',
+			'Ч' : '4',
+			'Ш' : 'W',
+			'Щ' : 'W',
+			'Ь' : 'b',
+			'Ы' : 'bI',
+			'Ъ' : 'b',
+			'Э' : '3',
+			'Ю' : 'I0',
+			'Я' : 'R'
+		},
+		getWastified : function(s) {
+			var ans = "";
+			var rep = this.replaces;
+			var englishRegexp = /[a-z]/;
+			for (var i = 0; i < s.length; i++) {
+				var c = s[i];
+				if (!englishRegexp.test(c)) {
+					c = c.toUpperCase();
+				}
+				if (rep[c] === null || rep[c] === undefined) {
+					ans += c;
+				} else {
+					ans += rep[c];
+				}
+			}
+			return ans;
+		}
+	},
+
+	"vc" : {
+		replaces : {
+			"м" : "m",
+			"й" : "и",
+			"д" : "g",
+			"а" : "ɑ"
+		},
+		getWastified : function(s) {
+			var ans = "";
+			var rep = this.replaces;
+			for (var i = 0; i < s.length; i++) {
+				var c = s[i];
+				if (rep[c] === null || rep[c] === undefined) {
+					ans += c;
+				} else {
+					ans += rep[c];
+				}
+			}
+			return ans;
+		}
+	},
+	"vc_alt" : {
+		replaces : {
+			"м" : "m",
+			"п" : "n",
+			"й" : "u",
+			"и" : "u",
+			"д" : "g",
+			"а" : "ɑ",
+		},
+		getWastified : function(s) {
+			var ans = "";
+			var rep = this.replaces;
+			for (var i = 0; i < s.length; i++) {
+				var c = s[i];
+				if (rep[c] === null || rep[c] === undefined) {
+					ans += c;
+				} else {
+					ans += rep[c];
+				}
+			}
+			return ans;
+		}
+	},
+	"sa_caps" : {
+		replaces : {
+			"G" : "Ж",
+			"R" : "Я",
+			"S" : "Ы",
+			"W" : "Ш",
+
+			"Э" : "3",
+			"Щ" : "Ш",
+			"Ъ" : "Ь",
+			"Ё" : "Е",
+			"Й" : "И",
+
+			"4" : "Ч",
+			"6" : "Б"
+		},
+		getWastified : function(s) {
+			var ans = "";
+			var rep = this.replaces;
+			var englishRegexp = /[a-z]/;
+			for (var i = 0; i < s.length; i++) {
+				c = s[i];
+				console.log(englishRegexp.test(c) + " " + c);
+				if (!englishRegexp.test(c)) {
+					c = c.toUpperCase();
+				}
+				if (rep[c] === null || rep[c] === undefined) {
+					ans += c;
+				} else {
+					ans += rep[c];
+				}
+			}
+			return ans;
+		}
+	},
+	"full_width" : {
+		replaces : {
+			"!" : "！",
+			'"' : "＂",
+			"#" : "＃",
+			"$" : "＄",
+			"%" : "％",
+			"&" : "＆",
+			"'" : "＇",
+			"(" : "（",
+			")" : "）",
+			"*" : "＊",
+			"+" : "＋",
+			"," : "，",
+			"-" : "－",
+			"." : "．",
+			"/" : "／",
+			"0" : "０",
+			"1" : "１",
+			"2" : "２",
+			"3" : "３",
+			"4" : "４",
+			"5" : "５",
+			"6" : "６",
+			"7" : "７",
+			"8" : "８",
+			"9" : "９",
+			":" : "：",
+			";" : "；",
+			"<" : "＜",
+			"=" : "＝",
+			">" : "＞",
+			"?" : "？",
+			"@" : "＠",
+			"A" : "Ａ",
+			"B" : "Ｂ",
+			"C" : "Ｃ",
+			"D" : "Ｄ",
+			"E" : "Ｅ",
+			"F" : "Ｆ",
+			"G" : "Ｇ",
+			"H" : "Ｈ",
+			"I" : "Ｉ",
+			"J" : "Ｊ",
+			"K" : "Ｋ",
+			"L" : "Ｌ",
+			"M" : "Ｍ",
+			"N" : "Ｎ",
+			"O" : "Ｏ",
+			"P" : "Ｐ",
+			"Q" : "Ｑ",
+			"R" : "Ｒ",
+			"S" : "Ｓ",
+			"T" : "Ｔ",
+			"U" : "Ｕ",
+			"V" : "Ｖ",
+			"W" : "Ｗ",
+			"X" : "Ｘ",
+			"Y" : "Ｙ",
+			"Z" : "Ｚ",
+			"[" : "［",
+			"\\" : "＼",
+			"]" : "］",
+			"^" : "＾",
+			"_" : "＿",
+			"`" : "｀",
+			"a" : "ａ",
+			"b" : "ｂ",
+			"c" : "ｃ",
+			"d" : "ｄ",
+			"e" : "ｅ",
+			"f" : "ｆ",
+			"g" : "ｇ",
+			"h" : "ｈ",
+			"i" : "ｉ",
+			"j" : "ｊ",
+			"k" : "ｋ",
+			"l" : "ｌ",
+			"m" : "ｍ",
+			"n" : "ｎ",
+			"o" : "ｏ",
+			"p" : "ｐ",
+			"q" : "ｑ",
+			"r" : "ｒ",
+			"s" : "ｓ",
+			"t" : "ｔ",
+			"u" : "ｕ",
+			"v" : "ｖ",
+			"w" : "ｗ",
+			"x" : "ｘ",
+			"y" : "ｙ",
+			"z" : "ｚ",
+			"{" : "｛",
+			"|" : "｜",
+			"}" : "｝",
+			"~" : "～"
+
+		},
+		getWastified : function(s) {
+			var ans = "";
+			var rep = this.replaces;
+			for (var i = 0; i < s.length; i++) {
+				var c = s[i];
+				if (rep[c] === null || rep[c] === undefined) {
+					ans += c;
+				} else {
+					ans += rep[c];
+				}
+			}
+			return ans;
+		}
+	},
+	"double_underline" : {
+		adds : '\u0333',
+		getWastified : function(s) {
+			var ans = "";
+			console.log(s);
+			console.log(this.adds);
+			for (var i = 0; i < s.length; i++) {
+				ans += this.adds + s[i];
+			}
+			return ans;
+		}
+	},
+
+}
---
+++ wastificator.user.js
@@ -0,0 +1,212 @@
+// ==UserScript==
+// @name        Wastificator
+// @namespace   ru.recursivepie.wastificator
+// @require 	https://raw.githubusercontent.com/RecursivePie/Wastificator/master/styles.js
+// @include     *
+// @version     6.3
+// @grant       none
+//
+// @author      RecursivePie (recursivepie@gmail.com)
+// ==/UserScript==
+
+var state = {
+	currentTextarea : null,
+	useVal : null,
+	lastSelectedText : null,
+}
+
+var elements = {
+	div : null,
+	button : null,
+	contextTextArea : null,
+	put : null,
+	contextMenu : null,
+}
+
+function attachToTextarea(ta) {
+	closeContextMenu();
+	state.currentTextarea = ta;
+	if (ta.value === null || ta.value === undefined) {
+		state.useVal = false;
+		elements.contextTextArea.value = ta.textContent || ta.innerText;
+	} else {
+		state.useVal = true;
+		elements.contextTextArea.value = ta.value;
+	}
+	ta.parentNode.insertBefore(elements.div, ta.nextSibling);
+}
+
+function onDocumentFocused(evt) {
+	var target = evt.srcElement || evt.originalTarget;
+	if (target
+			&& (target.nodeName.toUpperCase() == 'TEXTAREA' || target.contentEditable === 'true')
+			&& (elements.contextMenu.contains(target) === false)) {
+		if (state.currentTextarea != target) {
+			attachToTextarea(target);
+		}
+	}
+}
+
+function wastifyText(ta, styleName) {
+	var style = styles[styleName];
+	var val = "";
+	if (state.useVal) {
+		val = ta.value;
+	} else {
+		val = ta.textContent || ta.innerText;
+	}
+	var ans = style.getWastified(val);
+	if (state.useVal) {
+		ta.value = ans;
+	} else {
+		while (ta.firstChild !== null)
+			ta.removeChild(ta.firstChild); // remove all existing content
+		ta.appendChild(document.createTextNode(ans));
+	}
+}
+
+function buttonClicked(evt) {
+	if (elements.contextMenu.style.display == "none") {
+		showContextMenu();
+	} else {
+		closeContextMenu();
+	}
+}
+
+function styleClicked(evt) {
+	var target = evt.srcElement || evt.originalTarget;
+	var e = elements.contextTextArea;
+	var val = e.value.substring(e.selectionStart, e.selectionEnd);
+	var wastified = styles[target.textContent].getWastified(val);
+	var len = wastified.length;
+	var full = e.value.substring(0, e.selectionStart) + wastified
+			+ e.value.substring(e.selectionEnd);
+	e.value = full;
+}
+
+function showContextMenu() {
+	if (state.useVal) {
+		elements.contextTextArea.value = state.currentTextarea.value;
+	} else {
+		elements.contextTextArea.value = state.currentTextarea.textContent
+				|| state.currentTextarea.innerText;
+	}
+	elements.contextMenu.style.display = "inline-block";
+	elements.contextMenu.style.position = "absolute";
+	if (offscreen(elements.contextMenu)) {
+		elements.contextMenu.style.position = "";
+	}
+}
+
+function closeContextMenu() {
+	elements.contextMenu.style.display = "none";
+}
+
+function onDocumentClicked(evt) {
+	var target = evt.srcElement || evt.originalTarget;
+	if (target != elements.button
+			&& elements.contextMenu.contains(target) === false) {
+		closeContextMenu();
+	} else {
+	}
+
+}
+
+function putClicked() {
+	var text = elements.contextTextArea.value;
+	if (state.useVal) {
+		state.currentTextarea.value = text;
+	} else {
+		// remove existing content
+		while (state.currentTextarea.firstChild !== null)
+			state.currentTextarea.removeChild(state.currentTextarea.firstChild);
+		state.currentTextarea.appendChild(document.createTextNode(text));
+	}
+	closeContextMenu();
+}
+
+function offscreen(el) {
+	var wh = window.innerHeight;
+	var ww = window.innerWidht;
+	var rect = el.getBoundingClientRect();
+	var x = rect.x;
+	var y = rect.y;
+	var top = rect.top;
+	var left = rect.left;
+	var ew = rect.width;
+	var eh = rect.height;
+	if (y < 0) {
+		console.log("y < 0");
+		return true;
+	}
+	if (x < 0) {
+		console.log("x < 0");
+		return true;
+	}
+	if (x + ew > ww) {
+		console.log("x + ew > ww");
+		return true;
+	}
+	if (y + eh > wh) {
+		console.log("y + eh > wh");
+		console.log("y : " + y, "eh : " + eh, "wh : " + wh);
+		return true;
+	}
+};
+
+function init() {
+
+	elements.div = document.createElement('div');
+
+	elements.button = document.createElement('button');
+	elements.button.innerHTML = "W";
+	elements.button.addEventListener("click", buttonClicked, true);
+
+	elements.div.appendChild(elements.button);
+	elements.div.appendChild(document.createElement("br"));
+
+	elements.contextMenu = document.createElement("div");
+	elements.contextMenu.style.position = "absolute";
+	elements.contextMenu.style.zIndex = 777;
+	elements.contextMenu.style.border = "solid 1px red";
+	elements.contextMenu.style.display = "none";
+	elements.contextMenu.style.backgroundColor = "white";
+	elements.contextMenu.style.padding = "0px";
+	elements.contextMenu.style.margin = "0px";
+	var styleList = document.createElement("ol");
+	for ( var prop in styles) {
+		var entry = document.createElement("li");
+		var link = document.createElement("a");
+		link.style.cursor = "pointer";
+		link.style.color = "blue";
+		link.appendChild(document.createTextNode(prop));
+		link.addEventListener("click", styleClicked, true);
+		entry.appendChild(link);
+		styleList.appendChild(entry);
+	}
+	elements.contextMenu.appendChild(styleList);
+
+	elements.contextTextArea = document.createElement('textarea');
+	elements.contextTextArea.tabIndex = 322;
+	elements.contextTextArea.style.resize = "both";
+	elements.contextTextArea.style.margin = "2px";
+
+	elements.put = document.createElement('button');
+	elements.put.tabIndex = 323;
+	elements.put.innerHTML = "PUT";
+	elements.put.style.width = "100%";
+	elements.put.style.display = "block";
+	elements.put.addEventListener("click", putClicked, true);
+
+	elements.contextMenu.appendChild(elements.contextTextArea);
+
+	elements.contextMenu.appendChild(elements.put);
+
+	elements.div.appendChild(elements.contextMenu);
+
+	document.addEventListener('focus', onDocumentFocused, true);
+	document.addEventListener('click', onDocumentClicked, true);
+
+}
+
+init();
