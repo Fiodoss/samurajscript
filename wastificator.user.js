25acd57a22161790fd7dc2447afb7dee1af4b33f
Ilgiz Mustafin <ilgimustafin@gmail.com>
2015-03-02 01:05:39 +0300

    Added word replacements for sa_caps

--- wastificator.user.js
+++ wastificator.user.js
@@ -3,7 +3,7 @@
 // @namespace   ru.recursivepie.wastificator
 // @require 	https://raw.githubusercontent.com/RecursivePie/Wastificator/master/styles.js
 // @include     *
-// @version     6.4
+// @version     6.5
 // @grant       none
 //
 // @author      RecursivePie (recursivepie@gmail.com)
